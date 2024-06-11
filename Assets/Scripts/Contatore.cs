2cbf6ebff99bf72b37e31368ee42b25f10b20498
frebb01 <f.cantiello30@gmail.com>
2024-06-11 19:52:59 +0200

    Animazione contatore

--- Assets/Scripts/Contatore.cs
+++ Assets/Scripts/Contatore.cs
@@ -6,12 +6,17 @@ public class Contatore : MonoBehaviour, InteractInterface
 {
     [SerializeField] private string _prompt;
     [SerializeField] public SceneState sceneState;
+    [SerializeField] Animator _animator;
     public string InteractionPrompt => _prompt;
 
     public bool Interact(Interactor interactor)
     {
         if (sceneState._hasKey == true)
+        {
             Debug.Log("Sportello aperto!");
+            _animator.SetBool("key", true);
+        }
+            
         else Debug.Log("Trova chiave per aprire lo sportello");
         return true;
     }
