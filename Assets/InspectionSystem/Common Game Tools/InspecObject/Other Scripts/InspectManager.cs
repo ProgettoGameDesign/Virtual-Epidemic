b0cce510f879c719f62df922db02b1f843ba32ed
Faber-Z16 <135551994+Faber-Z16@users.noreply.github.com>
2024-06-27 17:21:33 +0200

    Inspector con esempio zainetto

---
+++ Assets/InspectionSystem/Common Game Tools/InspecObject/Other Scripts/InspectManager.cs
@@ -0,0 +1,188 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace CGT
+{
+    [ExecuteInEditMode]
+    public class InspectManager : MonoBehaviour
+    {
+        public bool blurOnInspect = true;
+        public Camera mainCamera;
+        public Camera inspectCamera;
+        public bool autoCullingMask = true;
+
+        private InspectObject inspecting=null;
+        private float initFOV;
+
+        private static string inspectedLayer = "CGT_Inspect";
+        private int layerInspect;
+
+        private static InspectManager _instance = null;
+        public static InspectManager instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = (InspectManager)FindObjectOfType(typeof(InspectManager));
+                    if (_instance == null)
+                    {
+                        GameObject managers = GameObject.Find("/CGTManagers");
+                        if (managers == null)
+                            managers = new GameObject("CGTManagers");
+                        _instance = (new GameObject("InspectManager")).AddComponent<InspectManager>();
+                        _instance.transform.parent = managers.transform;
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        private void Awake()
+        {            
+#if UNITY_EDITOR
+            CreateLayer(inspectedLayer);
+#endif
+        }
+
+        public bool IsInspecting()
+        {
+            return inspecting!=null;
+        }
+
+        public int GetLayerInspect()
+        {
+            return layerInspect;
+        }
+
+        private void Start()
+        {
+            if(mainCamera==null)
+                mainCamera = Camera.main;
+
+            //If no examine camera set at all, the display a error
+            if (inspectCamera == null)
+                Debug.LogError("No inspect cam found!! \nPlease set the inspect camera in the script.");
+            else
+                initFOV = inspectCamera.fieldOfView;
+
+            layerInspect = LayerMask.NameToLayer(inspectedLayer);
+
+            if (Application.isPlaying)
+            {
+                //Wait for all objects to be set
+                StartCoroutine(LateStart(0.1f));
+
+                if (mainCamera.gameObject.GetComponent<CustomOutline>() == null)
+                {
+                    CustomOutline co = mainCamera.gameObject.AddComponent<CustomOutline>();
+                    co.width = 5;
+                }
+                else
+                    mainCamera.gameObject.GetComponent<CustomOutline>().enabled = true;
+            }
+        }
+
+        private void Update()
+        {
+            if (autoCullingMask)
+            {
+                //Set layer mask in main camera
+                mainCamera.cullingMask = mainCamera.cullingMask & ~(1 << layerInspect);
+
+                //Set layer mask in inspect camera
+                if (inspectCamera != null)
+                {
+                    inspectCamera.cullingMask = (1 << layerInspect);
+                    inspectCamera.clearFlags = CameraClearFlags.Depth;
+                }
+            }
+        }
+
+        IEnumerator LateStart(float waitTime)
+        {
+            yield return new WaitForSeconds(waitTime);
+        }
+
+        public void Ping()
+        {
+
+        }
+
+        public void StartInspecting(InspectObject io)
+        {
+            if (inspecting!=null)
+                return;
+            inspecting = io;
+
+            inspecting.gameObject.SetActive(true);
+            inspectCamera.fieldOfView = initFOV;
+
+            if (blurOnInspect)
+            {
+                if (mainCamera.gameObject.GetComponent<CustomBlur>() == null)
+                {
+                    CustomBlur cb = mainCamera.gameObject.AddComponent<CustomBlur>();
+                    cb.blurSize = 10;
+                }
+                else
+                    mainCamera.gameObject.GetComponent<CustomBlur>().enabled = true;
+            }
+        }
+
+        public void StopInspecting()
+        {
+            if (inspecting==null)
+                return;            
+
+            if(mainCamera.gameObject.GetComponent<CustomBlur>()!=null && blurOnInspect)
+                mainCamera.gameObject.GetComponent<CustomBlur>().enabled = false;
+
+            inspecting.gameObject.SetActive(false);
+            inspecting = null;
+        }
+
+#if UNITY_EDITOR
+        public static void CreateLayer(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new System.ArgumentNullException("name", "New layer name string is either null or empty.");
+
+            var tagManager = new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+            var layerProps = tagManager.FindProperty("layers");
+            var propCount = layerProps.arraySize;
+
+            SerializedProperty firstEmptyProp = null;
+
+            for (var i = 0; i < propCount; i++)
+            {
+                var layerProp = layerProps.GetArrayElementAtIndex(i);
+
+                var stringValue = layerProp.stringValue;
+
+                if (stringValue == name) return;
+
+                if (i < 8 || stringValue != string.Empty) continue;
+
+                if (firstEmptyProp == null)
+                    firstEmptyProp = layerProp;
+            }
+
+            if (firstEmptyProp == null)
+            {
+                UnityEngine.Debug.LogError("Maximum limit of " + propCount + " layers exceeded. Layer \"" + name + "\" not created.");
+                return;
+            }
+
+            firstEmptyProp.stringValue = name;
+            tagManager.ApplyModifiedProperties();
+        }
+#endif
+
+    }
+}
---
+++ Assets/InspectionSystem/Common Game Tools/InspecObject/Shaders/CustomBlur.shader
@@ -0,0 +1,66 @@
+﻿Shader "CGT/CustomBlur"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_BlurSize("Blur Size", Range(0,15)) = 10	
+	}
+	SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+			
+			sampler2D _MainTex;			
+			uniform int _BlurSize;
+			uniform float4 _MainTex_TexelSize;
+
+			fixed4 frag(v2f i) : SV_Target{
+			
+				//init color variable
+				float measurments = 1;
+				float4 col = tex2D(_MainTex, i.uv);
+				for(float index=1; index<_BlurSize; index++) {
+					float uvx = _MainTex_TexelSize.x*index;
+					float uvy = _MainTex_TexelSize.y*index;
+					col += tex2D(_MainTex, i.uv+float2(uvx,uvy));
+					col += tex2D(_MainTex, i.uv+float2(uvx,-uvy));
+					col += tex2D(_MainTex, i.uv+float2(-uvx,uvy));
+					col += tex2D(_MainTex, i.uv+float2(-uvx,-uvy));
+                    measurments += 4;
+				}
+				col = col / measurments;
+				
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
