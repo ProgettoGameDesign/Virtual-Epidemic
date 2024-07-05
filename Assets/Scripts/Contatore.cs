1b07ce3b942d5a1b4912c56c2de8a3a8bf758c41
gspeg <105271853+gspeg@users.noreply.github.com>
2024-07-05 12:24:35 +0200

    audio
    fixes

--- Assets/Scripts/Contatore.cs
+++ Assets/Scripts/Contatore.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using FMODUnity;
+using FMOD.Studio;
+using FMOD;
+using System.Linq;
 
 public class Contatore : MonoBehaviour, InteractInterface
 {
+
     [SerializeField] private string _prompt;
     [SerializeField] public SceneState sceneState;
     [SerializeField] private Animator _animator;
@@ -15,10 +20,22 @@ public class Contatore : MonoBehaviour, InteractInterface
     [SerializeField] private Button textcanvas;
     [SerializeField] private GameObject canvasContatore;
     //[SerializeField] private GameObject _NPC;
+    public EventInstance eventInstance;
+    public string eventpath = "";
 
     public string InteractionPrompt => _prompt;
+    private StudioEventEmitter mainCameraEmitter;
+   
+    private float targetVolume = -24.0f;  // Volume di destinazione (ad esempio abbassare a 20%)
     
 
+       void Start()
+    {
+        // Ottieni il riferimento al FMOD Event Emitter sulla Main Camera
+        mainCameraEmitter = Camera.main.GetComponent<StudioEventEmitter>();
+
+    }
+    
     public bool Interact(Interactor interactor)
     {
         if (!_animator.GetBool("key"))
@@ -26,6 +43,7 @@ public class Contatore : MonoBehaviour, InteractInterface
             _animator.SetBool("key", true);
             gameObject.GetComponent<CanvasActive>().enabled = false;
             //canvasContatore.SetActive(false);
+            SoundCoperchio();
             textcanvas.GetComponentInChildren<TextMeshProUGUI>().text = "(E) Accendi";
         }
         else
@@ -34,7 +52,7 @@ public class Contatore : MonoBehaviour, InteractInterface
             gameObject.layer = 0;
             gameObject.GetComponent<Outline>().enabled = false;
             gameObject.GetComponent<LightContatore>().enabled = false;
-            Debug.Log("Hai acceso la luce!");
+            UnityEngine.Debug.Log("Hai acceso la luce!");
             sceneState.blockMovementPlayer = true;
             _lightTransition.SetActive(true);
             _lightTransition.GetComponent<Animator>().SetBool("trigger",true);
@@ -47,10 +65,40 @@ public class Contatore : MonoBehaviour, InteractInterface
     private void ActiveLight()
     {
         sceneState._lightup = true;
+        LightSound();
+        AdjustVolume();
+      
+
     }
     private void SwitchTarget()
     {
         cameraSwitchTarget.SwitchToNPCTarget();
         _NPCanimator.SetBool("trigger", true);
     }
+
+    private void LightSound()
+    {
+       
+       eventInstance = RuntimeManager.CreateInstance(eventpath);
+       eventInstance.setParameterByName("Tipo", 0);
+       eventInstance.start();
+       eventInstance.release();
+
+    }
+    private void SoundCoperchio()
+    {
+        eventInstance = RuntimeManager.CreateInstance(eventpath);
+        eventInstance.setParameterByName("Tipo", 1);
+        eventInstance.start();
+        eventInstance.release();
+
+    }
+
+    private void AdjustVolume()
+    {
+        mainCameraEmitter.SetParameter("Volume", targetVolume);
+        mainCameraEmitter.SetParameter("VolumeRamp", 1 );
+        
+    }
+  
 }
